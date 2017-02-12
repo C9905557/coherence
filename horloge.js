412fe7f2cc887c86b23a180d2ae6e88bd6467ec4
Jean-Pierre Bernard <jeanpierre.bernard@gmail.com>
2017-02-12 15:12:13 -0500

    Ajout de terminer et de la durée.

--- horloge.js
+++ horloge.js
@@ -1,41 +1,78 @@
-var timeoutMinutes = 15;
+var timeoutMinutes = 1;
 var clockObj = null;
-var noSleep = new NoSleep();
+var gongObj = null;
+var noSleepObj = new NoSleep();
 
-function startCk() {
-    setBell(timeoutMinutes);
-    if( !clockObj ) {
-        clockObj = new CreateClock();
+function start() {
+    var tm = document.getElementById('timeoutMinutes');
+    timeoutMinutes = tm.value;
+    tm.readOnly = true;
+    gongObj = new Gong();
+    gongObj.setGong(timeoutMinutes);
+    if( !clockObj ) clockObj = new CreateClock();
         clockObj.displayClock();
-    }
 };
 
-function setBell( timeout ) {
-    noSleep.enable();
-    setTimeout(function(){
-        var audio = new Audio('http://soundbible.com//mp3/Zen Buddhist Temple Bell-SoundBible.com-331362457.mp3');
-        audio.play();
-        noSleep.disable();
-     }, timeoutMinutes*60*1000 + 10*1000 +1);
-    <!-- http://soundbible.com//mp3/Bleep-SoundBible.com-1927126940.mp3 -->
-    <!-- <embed src="specPlayer.swf" flashvars="song=/mp3/Zen Buddhist Temple Bell-SoundBible.com-331362457.mp3" quality="high" bgcolor="#FFFFFF" name="specPlayer" allowscriptaccess="sameDomain" allowfullscreen="true" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" width="185" height="85" align="middle"> -->
+function stop() {
+  var tm = document.getElementById('timeoutMinutes');
+  tm.readOnly = false;
+  gongObj.stopGong();
+  clockObj.clearClock();
+  noSleepObj.disable();
+}
+
+function Gong( timeout ) {
+    var timerId;
+
+    this.setGong = function () {
+        noSleepObj.enable();
+        timerId = setTimeout(function(){
+                var audio = new Audio('https://soundbible.com//mp3/Zen%20Buddhist%20Temple%20Bell-SoundBible.com-331362457.mp3');
+                audio.play();
+                noSleepObj.disable();
+             }, timeoutMinutes*60*1000 + 10*1000 +1);
+            <!-- http://soundbible.com//mp3/Bleep-SoundBible.com-1927126940.mp3 -->
+            <!-- <embed src="specPlayer.swf" flashvars="song=/mp3/Zen Buddhist Temple Bell-SoundBible.com-331362457.mp3" quality="high" bgcolor="#FFFFFF" name="specPlayer" allowscriptaccess="sameDomain" allowfullscreen="true" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" width="185" height="85" align="middle"> -->
+   }
+
+
+    this.stopGong = function () {
+        clearTimeout(timerId);
+    };
+
 }
 
 function CreateClock() {
     var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    var radius = canvas.height / 2;
-    ctx.translate(radius, radius);
-    radius = radius * 0.90
-    //var hour = 6 ; //now.getHours();
-    var minute = 59 //now.getMinutes();
-    var second = 49 //now.getSeconds();
-    var showMinuteHand = false;
+    var ctx, radius;
+    var minute, second, showMinuteHand;
+    var timerId;
 
     this.displayClock = function () {
-        setInterval(drawClock, 1000);
+        ctx = canvas.getContext("2d");
+        radius = canvas.height / 2;
+        ctx.translate(radius, radius);
+        radius = radius * 0.90
+        //var hour = 6 ; //now.getHours();
+        minute = 59 //now.getMinutes();
+        second = 49 //now.getSeconds();
+        showMinuteHand = false;
+
+        timerId = setInterval(drawClock, 1000);
     } ;
 
+    this.clearClock = function() {
+        clearInterval(timerId);
+
+        //ctx.beginPath();    // clear existing drawing paths
+        //ctx.save();         // store the current transformation matrix
+
+        // Use the identity matrix while clearing the canvas
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        //ctx.restore();        // restore the transform
+    };
 
     drawClock = function () {
       drawFace(ctx, radius);
